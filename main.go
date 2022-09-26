0f95517085ffe584d901c1d337e416e174833f14
SantiagoPeres <santiagogperes@gmail.com>
2022-09-26 13:46:35 +0200

    trash commit

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
+// package main
+
+// import (
+// 	"log"
+// 	"net/http"
+
+// 	"github.com/aprendagolang/webform/handlers"
+// )
+
+// func main() {
+// 	http.HandlerFunc("/", handlers.SubscriptionHandler())
+
+// 	if err := http.ListenAndServe(":8080", nil); err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// }
+
+// // um teste sobre o que fazer quando esta fazendo algo planejando um script para desenvolver commits com pequenas alterações
+
 package main
 
 import (
