80073694d309157c4b8161133ae84c0a5ce804fb
SantiagoPeres <santiagogperes@gmail.com>
2022-10-04 16:51:40 +0200

    fixed

--- db/connection.go
+++ db/connection.go
@@ -9,6 +9,21 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// func getConnection() (client *mongo.client, ctx context.Context) {
+// 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+// 	err = client.Connect(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	return
+// }
+
 func getConnection() (client *mongo.client, ctx context.Context) {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
